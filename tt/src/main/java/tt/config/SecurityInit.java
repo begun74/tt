67d0900c605a94f4f505d917ae2d37693b4761c8
tempmin <tempmin@sysadm.minoblsp.by>
2016-10-18 14:45:02 +0300

    1

---
+++ tt/src/main/java/tt/config/SecurityInit.java
@@ -0,0 +1,7 @@
+package tt.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer{
+
+}
